4242fc616f1f08d507f2e5937ee230300a296261
Eduardo Henrique de França Oliveira <eduhenriqueoliveira9@gmail.com>
2023-06-22 00:27:16 -0300

    Derivadas

--- codigo.c
+++ codigo.c
@@ -24,31 +24,69 @@ double funcPX(double x){
     return tan(x);
 }
 double funcQX(double x){
-    return x;
+    double retorno = sin(sqrt(x)/pow(x,2));
+    return retorno;
 }
 double funcRX(double x){
-
+    return (pow(x,2)-x);
 }
 
-double derivadaAvancada(double x){
-    double funcDeri = (funcGX(x+H)-funcGX(x))/H;
+double derivadaAvancada(double x, int opc){
+    double funcDeri;
+    switch (opc){
+        case 1:
+            funcDeri = (funcFX(x+H)-funcFX(x))/H;
+            break;
+        case 2:
+            funcDeri = (funcQX(x+H)-funcQX(x))/H;
+            break;
+    }
+    //(funcRX(x+H)-funcRX(x))/H;
+    
     return funcDeri;
 }
 
-double derivadaRecuada(double x){
-    double funcDeri = (funcGX(x) - funcGX(x-H))/H;
+double derivadaRecuada(double x, int opc){
+    double funcDeri; 
+    switch (opc){
+        case 1:
+            funcDeri = (funcFX(x) - funcFX(x-H))/H;
+            break;
+        case 2:
+            funcDeri = (funcQX(x)-funcQX(x-H))/H;
+            break;
+    }
     return funcDeri;
 }
 
-double derivadaCentrada(double x){
-    double funcDeri = (funcGX(x+H) - funcGX(x-H))/(2*H);
+double derivadaCentrada(double x, int opc){
+    double funcDeri;
+        switch (opc){
+        case 1:
+            funcDeri = (funcFX(x+H) - funcFX(x-H))/(2*H);
+            break;
+        case 2:
+            funcDeri = (funcQX(x+H) - funcQX(x-H))/(2*H);
+            break;
+    }
     return funcDeri;
 }
 
 
 void main(){
-    double i=0;
-    i = funcGX(i);
-    printf("funciona %lf\n", i);
+    double d=1;
+    printf("Valor de entrada: %lf\n", d);
+    printf("Valor da função neste ponto: %lf", funcQX(d));
+
+    double i = derivadaAvancada(d,2);
+    printf("\nDerivada Avançada %lf\n", i);
+
+    i = derivadaRecuada(d, 2);
+    printf("Derivada Recuada %lf\n", i);
+
+    i = derivadaCentrada(d, 2);
+    printf("Derivada Centrada %0.13lf\n", i);
+    //1.8326571590206,
+    
 }
 
